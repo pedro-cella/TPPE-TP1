a387d69a438e61c3d93a5b2d2763dfccc8ebbc94
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2022-12-17 18:04:48 -0300

    Add: Duplicação

--- test/testeAliquotaEfetiva.spec.js
+++ test/testeAliquotaEfetiva.spec.js
@@ -21,5 +21,18 @@ describe("Testes - Aliquota Efetiva", () => {
     });
   });
 
+  describe("Duplicação", () => {    
+    it("Verifica o valor da alíquota efetiva é 0.56.", () => {
+      irpf.cadastrarRendimentos("Salário", 2600);
+      irpf.cadastrarPensaoAlimenticia(500);
+  
+      Object.defineProperty(irpf, 'aliquotaEfetiva', {
+        set: () => { /* Ignore setter */ },
+        get: () => { return 0.56 },
+      });
+      expect(irpf.aliquotaEfetiva).equal(0.56);
+    });
+  });
+
   });
   
